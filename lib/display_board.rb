a0d4d18ace0a0c3efce69d7ff9856a57dbd032f0
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-10 15:49:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,15 +1,9 @@
-def display_board(situations)
-  puts row_1 = [" ", "|", " ", "|", " "]
-  puts separator = "-----------"
-  puts row
-  puts separator
-  puts row
+def display_board(board)
+   puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+   puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def display_board
-  puts row = [" ", "|", " ", "|", " "]
-  puts separator = "-----------"
-  puts row
-  puts separator
-  puts row
-end
+display_board(board)
